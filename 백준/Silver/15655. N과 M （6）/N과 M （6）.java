b1a8654a5f290cbada066fb1dc779e665e7b3d34
나 현 <107095081+nahyun2kim@users.noreply.github.com>
2022-09-20 17:21:31 +0900

    [Silver III] Title: N과 M (6), Time: 112 ms, Memory: 12928 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	static int N, M;
+	static int[] nums, sel;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		nums = new int[N];
+		for(int i=0; i<N; i++)
+			nums[i] = sc.nextInt();
+		Arrays.sort(nums);
+		
+		sel = new int[M];
+		comb(0,0);
+		System.out.println(sb);
+		
+	}
+	
+	static void comb(int idx, int selIdx) {
+		if(selIdx == M) {
+			for(int s : sel)
+				sb.append(s).append(' ');
+			sb.append('\n');
+			return;
+		} else if(idx == N)
+			return;
+		
+		sel[selIdx] = nums[idx];
+		comb(idx+1, selIdx+1);
+		comb(idx+1, selIdx);
+	}
+}
