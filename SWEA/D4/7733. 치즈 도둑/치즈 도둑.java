3ec967cafbffef57e8e7f3c8f51b1a466798bdf8
나 현 <107095081+nahyun2kim@users.noreply.github.com>
2022-09-27 11:31:59 +0900

    [D4] Title: 치즈 도둑, Time: 349 ms, Memory: 127,592 KB -BaekjoonHub

--- SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
+++ SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
@@ -8,9 +8,12 @@ public class Solution {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
 		for(int tc=1; tc<=T; tc++) {
-			N = sc.nextInt();
+			
+			//1. 입력
+			N = sc.nextInt();       // N: 치즈 한변의 길이
 			cheese = new int[N][N];
-			X = 0; maxCnt = 1;
+			X = 0; 
+			maxCnt = 1; // ! 치즈는 무조건 한덩이부터 시작 !
 			for(int i=0; i<N; i++) {
 				for(int j=0; j<N; j++) {
 					cheese[i][j] = sc.nextInt();
@@ -18,8 +21,11 @@ public class Solution {
 				}
 			}
 			
+			//2. 최대 맛있는 정도의 날까지만 치즈 덩어리를 세보자
 			for(int i=1; i<X; i++) {
 				int ch = eatCheese(i);
+				
+				// 만약 요정이 먹지 않았다면 날을 넘기자
 				if (ch == 0)
 					continue;
 				arr = tmpArr();
@@ -33,9 +39,11 @@ public class Solution {
 						}
 					}
 				}
+				// 최대 덩어리의 개수 비교
 				maxCnt = Math.max(maxCnt, cnt);
 			}
 			
+			//3. 출력
 			System.out.printf("#%d %d\n", tc, maxCnt);
 			
 			
@@ -43,6 +51,7 @@ public class Solution {
 		}// test
 	}// main
 	
+	//요정이 날짜에 맞는 치즈를 먹음
 	static int eatCheese(int day) {
 		int count = 0;
 		for(int i=0; i<N; i++) {
@@ -56,6 +65,7 @@ public class Solution {
 		return count;
 	}
 	
+	// 덩어리의 개수를 세기 위해 임시로 사용할 치즈 배열 복사
 	static int[][] tmpArr(){
 		int[][] tmp = new int[N][N];
 		for(int i=0; i<N; i++) {
@@ -64,6 +74,7 @@ public class Solution {
 		return tmp;
 	}
 	
+	// 한 덩어리를 체크해줄 함수
 	static void check(int r, int c) {
 		for (int i=0; i<4; i++) {
 			int di = r + del[i][0];
