9c8085c091af943bf313e4bd619bff802f67e745
나 현 <107095081+nahyun2kim@users.noreply.github.com>
2022-09-27 15:53:05 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 163 ms, Memory: 38,188 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
 
+// check 필요없는데 왜했지...?
 public class Solution {
 	static int minCost;
 	static int[] voucher = new int[4]; 
 	static int[] month = new int[12];
-	static boolean[] check;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
 		for(int tc=1; tc<=T; tc++) {
+			
+			//1. 입력
 			for(int i=0; i<4; i++)
 				voucher[i] = sc.nextInt();
 			for(int i=0; i<12; i++)
@@ -16,9 +18,10 @@ public class Solution {
 			
 			minCost = voucher[3]; // 초기 최솟값을 연간 이용권으로 설정
 			
-			check = new boolean[12];
+			//2. 구매시작하자
 			voucherPerchase(0, 0);
 			
+			//3. 출력
 			System.out.printf("#%d %d\n", tc, minCost);
 			
 		}// test
@@ -32,25 +35,17 @@ public class Solution {
 		if(cost >= minCost)
 			return;
 		
+		// i=0이면 일일이용권, i=1이면 1개월이용권, i=2이면 3개월이용권
 		for(int i=0; i<3; i++) {
 			if (i == 0) {
-				check[mon] = true;
 				voucherPerchase(mon+1, cost+voucher[i]*month[mon]);
-				check[mon] = false;
 			}else if(i == 1) {
-				check[mon] = true;
 				voucherPerchase(mon+1, cost+voucher[i]);
-				check[mon] = false;
 			}else {
-				for(int m=mon; m<mon+3 && m<12; m++)
-					check[mon] = true;
 				int nm = mon+3;
-				if (nm > 12)
+				if (nm > 12)  // 달을 초과했다면 12월까지만 적용
 					nm = 12;
 				voucherPerchase(nm, cost+voucher[i]);
-				for(int m=mon; m<mon+3 && m<12; m++)
-					check[mon] = false;
-			
 			}
 		}
 	}
