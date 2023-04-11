390c2d444aa58016b710f6c29095b4d55a1cad25
나 현 <107095081+nahyun2kim@users.noreply.github.com>
2023-04-11 14:42:41 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 78.15 ms, Memory: 26.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,17 +1,7 @@
 def solution(participant, completion):
-    N = len(participant)
-    parti = {}
-    for i in range(N):
-        if participant[i] in parti.keys():
-            parti[participant[i]] += 1
-        else:
-            parti[participant[i]] = 1
-    
-    for name in completion:
-        if parti[name] == 1:
-            del(parti[name])
-        else:
-            parti[name] -= 1
-    name = list(map(str, parti.keys()))
-    answer = name[0]
-    return answer
+    participant.sort()
+    completion.sort()
+    for p, c in zip(participant, completion):
+        if p != c:
+            return p
+    return participant[-1]
