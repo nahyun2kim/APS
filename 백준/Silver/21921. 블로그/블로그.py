6680285bba405896a8f2857640ad0099b77facf9
나 현 <107095081+nahyun2kim@users.noreply.github.com>
2023-04-11 09:51:45 +0900

    [Silver III] Title: 블로그, Time: 384 ms, Memory: 187972 KB -BaekjoonHub

---
+++ 백준/Silver/21921. 블로그/블로그.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+# n: 블로그를 시작하고 지난 일수, x: 탐색할 기간
+n, x = map(int, input().split())
+
+# 방문자 수
+visit = list(map(int, input().split()))
+
+# 누적합 구하기
+visit_sum = [0, visit[0]]
+for i in range(1, n):
+    visit_sum.append(visit_sum[i] + visit[i])
+
+# 돌면서 최대값 구하기
+ans = []
+for i in range(x, n+1):
+    ans.append(visit_sum[i] - visit_sum[i-x])
+
+ans.sort(key = lambda x : -x)
+
+if ans[0] == 0:
+    print('SAD')
+else:
+    day = 0
+    answer = ans[0]
+    for a in ans:
+        if answer == a:
+            day += 1
+    print(answer)
+    print(day)
