6029ce69bfb8f176f42b87e2e787a1c5b02790b4
나 현 <107095081+nahyun2kim@users.noreply.github.com>
2023-04-17 22:19:34 +0900

    [level 2] Title: 타겟 넘버, Time: 2.27 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/README.md
+++ 프로그래머스/lv2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 1.67 ms
+메모리: 10 MB, 시간: 2.27 ms
 
 ### 구분
 
--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,19 +1,11 @@
 def solution(numbers, target):
     n = len(numbers)
-    # 누적합
-    cul_sum = [0]
-    for num in numbers:
-        cul_sum.append(cul_sum[-1] + num)
     answer = 0
     def dfs(idx, ans):
         nonlocal answer
         if idx == n:
             if ans == target:
                 answer += 1
-            return
-        if ans < target and ans + cul_sum[-1] - cul_sum[idx] < target:
-            return
-        if ans > target and ans - cul_sum[-1] + cul_sum[idx] > target:
             return
         
         dfs(idx + 1, ans + numbers[idx])
