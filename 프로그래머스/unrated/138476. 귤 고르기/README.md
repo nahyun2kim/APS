7d6991ad04ca0a1a71efcc324394a46c3c9a6981
나 현 <107095081+nahyun2kim@users.noreply.github.com>
2023-06-08 16:00:37 +0900

    [unrated] Title: 귤 고르기, Time: 14.68 ms, Memory: 17.9 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/README.md
+++ 프로그래머스/unrated/138476. 귤 고르기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 귤 고르기 - 138476 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476?language=python3) 
 
 ### 성능 요약
 
-메모리: 18 MB, 시간: 45.61 ms
+메모리: 17.9 MB, 시간: 14.68 ms
 
 ### 구분
 
--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
@@ -1,16 +1,8 @@
+import collections
 def solution(k, tangerine):
     cnt = 0
-    size = {}
-    for t in tangerine:
-        if t in size.keys():
-            size[t] += 1
-        else:
-            size[t] = 1
-    size_list = []
-    for v in size.values():
-        size_list.append(v)
-        
-    size_list.sort(reverse=True)
+    size = collections.Counter(tangerine)
+    size_list = sorted(size.values(), reverse=True)
     for i in range(len(size_list)):
         cnt += size_list[i]
         if cnt >= k:
