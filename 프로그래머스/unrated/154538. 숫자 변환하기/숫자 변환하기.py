cb120bdf9e8c87aa39aa005387d10e0255c7b29d
나 현 <107095081+nahyun2kim@users.noreply.github.com>
2023-06-30 01:42:40 +0900

    [unrated] Title: 숫자 변환하기, Time: 0.10 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.py
@@ -0,0 +1,15 @@
+from collections import deque
+def solution(x, y, n):
+    q = deque([(y, 0)])
+    while q:
+        num, cnt = q.popleft()
+        if num == x:
+            return cnt
+        elif num < x:
+            continue
+        if num % 3 == 0:
+            q.append((num // 3, cnt + 1))
+        if num % 2 == 0:
+            q.append((num // 2, cnt + 1))
+        q.append((num - n, cnt + 1))
+    return -1
