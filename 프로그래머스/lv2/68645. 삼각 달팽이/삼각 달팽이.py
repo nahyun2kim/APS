73f12257c9824db79e4d08f0582d97113eb7a0fa
나 현 <107095081+nahyun2kim@users.noreply.github.com>
2023-08-16 23:37:18 +0900

    [level 2] Title: 삼각 달팽이, Time: 177.39 ms, Memory: 43.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.py
@@ -0,0 +1,22 @@
+def solution(n):
+    num = n * (n + 1) // 2
+    t = [[0]*i for i in range(1, n+1)]
+    delta = [[1, 0], [0, 1], [-1, -1]]
+    idx = 1
+    x = y = d = 0
+    while idx <= num:
+        t[x][y] = idx
+        nx = x + delta[d][0]
+        ny = y + delta[d][1]
+        if 0 <= nx < n and 0 <= ny <= nx and not t[nx][ny]:
+            x = nx
+            y = ny
+        else:
+            d = d + 1 if d < 2 else 0
+            x = x + delta[d][0]
+            y = y + delta[d][1]
+        idx += 1
+    answer = []
+    for t in t:
+        answer += t
+    return answer
