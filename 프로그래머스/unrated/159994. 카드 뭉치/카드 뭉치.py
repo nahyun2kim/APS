e290147add7cdeae730bb7e900f0fb6a3e4643e5
나 현 <107095081+nahyun2kim@users.noreply.github.com>
2023-09-20 14:45:29 +0900

    [unrated] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,9 @@
+def solution(cards1, cards2, goal):
+    for g in goal:
+        if cards1 and g == cards1[0]:
+            cards1.pop(0)
+        elif cards2 and g == cards2[0]:
+            cards2.pop(0)
+        else:
+            return 'No'
+    return 'Yes'
